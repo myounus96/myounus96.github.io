92d235b543d337845db06aa4c3395d092a2a3ab3
Muhammad Younus <myounus5396@gmail.com>
2020-07-11 20:45:04 +0500

    updated

---
+++ precache-manifest.6be2cccf9517a421964163b928d6556f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec1bf36d5e530bec783ddc42b8cb1e9",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "dd313066cfa85a059a17",
+    "url": "/developerFolio/static/css/3.48a93ee6.chunk.css"
+  },
+  {
+    "revision": "8fe8e244238b0ff7ef44",
+    "url": "/developerFolio/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "4fb1d7e71e2be1681e22",
+    "url": "/developerFolio/static/css/main.83752c5e.chunk.css"
+  },
+  {
+    "revision": "9c353b8245c384555cc6",
+    "url": "/developerFolio/static/js/2.147a64e1.chunk.js"
+  },
+  {
+    "revision": "dd313066cfa85a059a17",
+    "url": "/developerFolio/static/js/3.2f702ee0.chunk.js"
+  },
+  {
+    "revision": "8fe8e244238b0ff7ef44",
+    "url": "/developerFolio/static/js/4.81ecb125.chunk.js"
+  },
+  {
+    "revision": "4fb1d7e71e2be1681e22",
+    "url": "/developerFolio/static/js/main.d045d080.chunk.js"
+  },
+  {
+    "revision": "0faeb1f81419de1b9ab1",
+    "url": "/developerFolio/static/js/runtime-main.6f1971cc.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/developerFolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/developerFolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/developerFolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/developerFolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/developerFolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/developerFolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/developerFolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/developerFolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/developerFolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/developerFolio/static/media/talksCardBack.de3da806.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "developerFolio/precache-manifest.4c7f9cada6c5f28145374976dc5df28a.js"
+  "/developerFolio/precache-manifest.6be2cccf9517a421964163b928d6556f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("developerFolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
